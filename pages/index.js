c2036924b235ed7859b9a0ac8774b522104c39c4
kims19201080 <100283483+kims19201080@users.noreply.github.com>
2023-06-15 21:42:48 +0900

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -7,7 +7,7 @@ export default function Home() {
     <!DOCTYPE html>
 <html>
 <title>hizzal storage main page</title>
-<!--야미하구만-->
+<!--테스트-->
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
